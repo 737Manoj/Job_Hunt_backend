1d332561556f5a8fdd42b00fbfa529918bb17200
Manoj chauhan <mnojkumar0304@gmail.com>
2025-02-08 03:13:33 +0530

    Updated CORS config

--- src/main/java/com/Manoj/Job_Portal_Application/configuration/CorsConfig.java
+++ src/main/java/com/Manoj/Job_Portal_Application/configuration/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.Manoj.Job_Portal_Application.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
